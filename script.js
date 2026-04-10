8cf783e8763a355f2e1eb2a0ff2d67be5a331ef4
SFandiv <fandinosantiago10@gmail.com>
2026-04-10 11:53:41 -0500

    eliminación de funciones duplicadas dibujar y bresenham

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ function limpiarCanvas() {
  */
 function dibujar() {
     limpiarCanvas();
+    dibujarEscala();
+
+    let x0= parseInt(document.getElementById("x0").value);
+    let y0= parseInt(document.getElementById("y0").value);
+    let x1= parseInt(document.getElementById("x1").value);
+    let y1= parseInt(document.getElementById("y1").value);
+
+    bresenham(x0, y0, x1, y1, plot);
 /**
  * Dibuja un "pixel" en el canvas
  * @param {number} x 
@@ -24,41 +32,6 @@ function plot(x, y) {
 /**
  * Implementación del algoritmo de Bresenham.
  */
-function bresenham(x0, y0, x1, y1, plot) {
-    let dx = Math.abs(x1 - x0);
-    let dy = Math.abs(y1 - y0);
-    let sx = (x0 < x1) ? 1 : -1;
-    let sy = (y0 < y1) ? 1 : -1;
-    let err = dx - dy;
-
-    while (true) {
-
-        plot(x0, y0);
-
-        if (x0 === x1 && y0 === y1) break;
-        let e2 = 2 * err;
-        if (e2 > -dy) {
-            err -= dy;
-            x0 += sx;
-        }
-        if (e2 < dx) {
-            err += dx;
-            y0 += sy;
-        }
-    }
-}
-function dibujar() {
-    limpiarCanvas();
-    limpiarCanvas();
-    dibujarEscala();
-
-    let x0= parseInt(document.getElementById("x0").value);
-    let y0= parseInt(document.getElementById("y0").value);
-    let x1= parseInt(document.getElementById("x1").value);
-    let y1= parseInt(document.getElementById("y1").value);
-
-    bresenham(x0, y0, x1, y1, plot);
-}
 function bresenham(x0, y0, x1, y1, plot) {
     let tabla = document.getElementById("tabla");
     tabla.innerHTML = "";
