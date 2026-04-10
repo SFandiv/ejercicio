1bb667d727b62e53e0d856520a8d0c365d27d565
SFandiv <fandinosantiago10@gmail.com>
2026-04-10 11:27:18 -0500

    implementación del algoritmo de Bresenham con documentación

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const ctx = canvas.getContext("2d");
 function limpiarCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-
 /**
  * Función principal que se ejecuta al presionar el botón
  */
@@ -20,5 +19,31 @@ function dibujar() {
  */
 function plot(x, y) {
     ctx.fillRect(x, canvas.height - y, 1, 1);
+}
+/**
+ * Implementación del algoritmo de Bresenham.
+ */
+function bresenham(x0, y1, x1, y0, plot) {
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+    let sx = (x0 < x1) ? 1 : -1;
+    let sy = (y0 < y1) ? 1 : -1;
+    let err = dx - dy;
+
+    while (true) {
+        
+        plot(x0, y0);
+
+        if (x0 === x1 && y0 === y1) break;
+        let e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
 }
 }
