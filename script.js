f726857e8af4d8a1799482edff4e2bb610507068
SFandiv <fandinosantiago10@gmail.com>
2026-04-10 11:40:43 -0500

    generación de tabla paso a paso del algoritmo

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function plot(x, y) {
 /**
  * Implementación del algoritmo de Bresenham.
  */
-function bresenham(x0, y1, x1, y0, plot) {
+function bresenham(x0, y0, x1, y1, plot) {
     let dx = Math.abs(x1 - x0);
     let dy = Math.abs(y1 - y0);
     let sx = (x0 < x1) ? 1 : -1;
@@ -55,5 +55,37 @@ function dibujar() {
     let y1= parseInt(document.getElementById("y1").value);
 
     bresenham(x0, y0, x1, y1, plot);
+}
+function bresenham(x0, y0, x1, y1, plot) {
+    let tabla = document.getElementById("tabla");
+    tabla.innerHTML = "";
+
+    let dx= Math.abs(x1 - x0);
+    let dy= Math.abs(y1 - y0);
+    let sx= (x0 < x1) ? 1 : -1;
+    let sy= (y0 < y1) ? 1 : -1;
+    let err= dx - dy;
+
+    while (true) {
+        plot(x0, y0);
+        // Agregar fila a la tabla
+        let fila = `<tr>
+                        <td>${x0}</td>
+                        <td>${y0}</td>
+                        <td>${err}</td>
+                    </tr>`;
+        tabla.innerHTML += fila;
+
+        if (x0 === x1 && y0 === y1) break;
+        let e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
 }
 }
